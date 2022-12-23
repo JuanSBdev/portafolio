6923c26541bc70cb37f95abfcd92f2c89c82563e
Juan SB <juansb.dev@gmail.com>
2022-12-23 01:53:58 -0300

    sc

--- src/components/Name.vue
+++ src/components/Name.vue
@@ -1,13 +1,14 @@
         <script setup>
         import {ref, onMounted, onUpdated} from 'vue'
-        let titi = true;
+        let titi = document.getElementsByClassName('titi');
+        titi = false;
 setTimeout(function() { titi = !titi; console.log(titi) }, 3000);      
         </script>
 
 <template>
     <div class="wraper">
         <h1>{ Juan Lluvia S.B. } </h1>
-        <h2>Desarrollador web <spam v-if="titi" > _ </spam>  </h2>
+        <h2>Desarrollador web <spam class="titi" v-if="titi" > _ </spam>  </h2>
     </div>
 </template>
 <style scoped>
