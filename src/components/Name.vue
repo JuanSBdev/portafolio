eb3f5e15f4a4a8d2f2bc9c5fbfd9b717328e9055
Juan SB <juansb.dev@gmail.com>
2022-12-23 01:44:18 -0300

    tc

--- src/components/Name.vue
+++ src/components/Name.vue
@@ -1,9 +1,20 @@
-<script setup> 
-</script>
+        <script setup>
+        import {ref, onMounted, onUpdated} from 'vue'
+        let tit = true;
+      onMounted(() => {
+        let  tit = false;
+            console.log(tit)
+        });
+        onUpdated(()=>{
+             tit = true;
+            console.log(tit)
+        })
+        </script>
+
 <template>
     <div class="wraper">
         <h1>{ Juan Lluvia S.B. } </h1>
-        <h2>Desarrollador web <div class="titila"> _ </div>  </h2>
+        <h2>Desarrollador web <spam > _ </spam>  </h2>
     </div>
 </template>
 <style scoped>
