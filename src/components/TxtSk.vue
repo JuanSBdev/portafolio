a474c46fc4dc17a9f8de89a6938366b3a74541d9
Juan SB <juansb.dev@gmail.com>
2023-03-31 17:50:20 -0300

    skills

--- src/components/TxtSk.vue
+++ src/components/TxtSk.vue
@@ -1,31 +1,37 @@
-<template>
+<template >
     <!--    <img class='img-back' src="https://mystickermania.com/cdn/stickers/outer-space/astronaut-planet-balloons-512x512.png" alt="">
 -->
-    <div class="wrapper">
-        <p>Los lenguajes y tecnologías que manejo son HTML5, CSS3, Sass, Flexbox, Grid, Bootstrap, JavaScript, BootstrapVue, Vue3, SEO, Git, GitHub, UX/UI. 
-            Así como tambien he hecho pequeños proyectos en React y WordPress.
-        </p>
-    </div>
+<div class="containe">
+    <p>Los lenguajes y tecnologías que utilizo para diseñar y maquetar son HTML5, CSS3, Sass, Flexbox, Grids y Bootstrap.</p>
+    <p> 
+        Programo con JavaScript Vanilla y en uno de sus frameworks Vue.js 3, aunque tambien realicé pequeños proyectos en React y un CMS WordPress.
+    </p>
+    <p>
+        capacitado en UX/UI, SEO y
+        manejo de versiones de mi codigo con Git y utilizo GitHub para guardar mis proyectos.
+        utilizacion de clientes FTP tales como FileZilla.
+
+    </p>
+
+</div>
 </template>
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Staatliches&display=swap');
-.wrapper{
-    font-family: 'Staatliches', cursive;
-
+.containe{
+    top: 6rem;
     position: relative;
+    width: 100%;
+    border-radius: 1rem;
     display: flex;
-    padding-top: 20%;
-    left: 2%;
-    width: 90%;
-    height: 60rem;
-    background-image: url('https://anthoncode.com/wp-content/uploads/2020/04/visual-studio-code-2020.png');
-    background-repeat: no-repeat;
-    background-position: left ;
-   
+    text-align: center;
 }
 p{
-    font-size: 3rem;
+    font-size: 1.5rem;
     color: antiquewhite;
+    padding: 5%;
+}
+img{
+    width: 10%;
 }
 .img-back{
     opacity: .9;
--- src/views/SkillsView.vue
+++ src/views/SkillsView.vue
@@ -1,11 +1,31 @@
 <script setup>
+import TxtSk from '../components/TxtSk.vue';
+import SkillsLogos from '../components/Skills.vue'
 </script>
 
 <template>
-<div class="contenedor">
-    
-</div>
+    <div class="container">
+        <TxtSk></TxtSk>
+    </div>
+    <div class="otro">
+        <SkillsLogos/>
+
+    </div>
 </template>
 <style scoped>
+.container{
+    position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
 
+}
+.otro{
+    position: relative;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding-right: 0%;
+}
 </style>
