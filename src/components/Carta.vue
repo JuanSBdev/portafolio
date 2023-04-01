35639eca732e3ee2ee27034477cf9ead1556b3dc
Juan SB <juansb.dev@gmail.com>
2023-04-01 02:31:06 -0300

    mejora

--- src/components/Carta.vue
+++ src/components/Carta.vue
@@ -31,12 +31,10 @@
 .foto img{
   border-radius: 6rem;
   border: 5px solid black;
+  width: 50%;
+  margin-bottom: 10%;
+  min-width: 130px;
 }
-  img{
-    width: 30%;
-    min-width: 200px;
-    padding-bottom: 3%;
-  }
   h3{
     font-size: 1.5rem;
   }
@@ -44,26 +42,14 @@
   left: 13%;
   top: 2rem;
   width: 70%;
-  padding: 3%;
   border: 5px solid rgb(5, 3, 3);
   border-radius: 1rem;
   background-color: aqua;
-  position: absolute;
   background: rgb(0,230,77);
 background: linear-gradient(13deg, rgba(0,230,77,1) 0%, rgba(18,230,198,1) 46%, rgba(66,219,66,1) 100%);
 
 }
-.row{
-  display: flex;
-  flex-flow: row wrap;
 
-}
-.row-2{
-  display: flex;
-  justify-content: space-around;
-  flex-flow: row wrap;
-
-}
 .foto{
   display: flex;
   flex-flow: column;
@@ -71,11 +57,6 @@ background: linear-gradient(13deg, rgba(0,230,77,1) 0%, rgba(18,230,198,1) 46%, 
   margin-left: 25%;
 }
 
-.info{
-  width: 45%;
-}
-.skills{
-  width: 45%;
-}
+
 
 </style>
