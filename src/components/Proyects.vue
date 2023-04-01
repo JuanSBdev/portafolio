456fb246bdfc51dd2e62e129e06750e9875f2583
Juan SB <juansb.dev@gmail.com>
2023-03-31 23:10:29 -0300

    sigue-proyectos

--- src/components/Proyects.vue
+++ src/components/Proyects.vue
@@ -1,9 +1,43 @@
+
 <template>
-    <div class="contenedor">
+    
+    <div class="contenedor-pro">
         <h5 class="h3-con">
             Proyectos
         </h5>
         <div class="wraper row">
+            <div class="calculadora col-2">
+                <h6>Calculadora</h6>
+                <a href="https://juansbdev.github.io/appCalculadora/">
+                    <img src="https://ayudahosting.online/wp-content/uploads/2021/02/031-calculator.png" alt="">
+                </a>
+            </div>
+            <div class="coderhouse col-2">
+                <h6>Proyecto Final Coderhouse</h6>
+                <a href="https://juansbdev.github.io/Proyecto-Coderhouse/">
+                    <img src="https://img.freepik.com/iconos-gratis/hamburguesa_318-192560.jpg?w=2000" alt="">
+                </a>
+            </div>
+            <div class="carta col-2">
+                <h6> App Mobile Carta 'TABAK'</h6>
+                <a href="https://juansbdev.github.io/carta/">
+                    <img src="https://juansbdev.github.io/carta/img/logo2-removebg-preview.png" alt="">
+                </a>
+
+            </div>
+            <div class="api col-2">
+                <h6>API  Movies fetch</h6>
+                <a href="https://juansbdev.github.io/API-Movies/">
+                    <img src="https://diogorodrigues.dev/assets/img/blog/fetch-api-is-new-old-version-of-ajax.jpg" alt="">
+                </a>
+            </div>
+            <div class="lista col-2">
+                <h6>To Do List.</h6>
+                <a href="https://juansbdev.github.io/listaDeTareas/">
+                    <img src="https://cdn-icons-png.flaticon.com/512/6194/6194029.png" alt="">
+                </a>
+
+            </div>
         </div>
     </div>
 </template>
@@ -13,5 +47,16 @@
         display: flex;
         justify-content: center;
         margin-top: 6rem;
+    }
+    img{
+        width: 60%;
+    }
+    .contenedor-pro{
+        margin-right: 1%;
+    }
+    .wraper{
+        padding-top: 3rem;
+        display: flex;
+        justify-content: center;
     }
 </style>
