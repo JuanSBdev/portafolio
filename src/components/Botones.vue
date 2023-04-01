0c2b719a5be5b24afef55cfa76e9547ac8df8f05
Juan SB <juansb.dev@gmail.com>
2023-04-01 04:54:09 -0300

    neon

---
+++ src/components/Botones.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container col-6">
+        <router-link class="col-12" to="Skills" >
+        <p>portfolio</p>
+      </router-link>
+      <router-link class="col-12" to="Cv">
+        <p>Download CV</p>
+      </router-link>
+
+    </div>
+</template>
+<style scoped>
+.container{
+    display: flex;
+    width: 100%;
+    flex-flow: column wrap;
+    align-items: center;
+
+}
+a{
+    display: flex;
+    font-size: 2rem;
+    justify-content: center;
+    text-decoration: none;
+
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,75 @@
 <script setup>
-import Carta from '../components/Carta.vue';
+import Carta from '../components/Carta.vue'
+import Botones from '../components/Botones.vue';
 </script>
 <template>
-    <div class="contenedor-home">
-      <h1>Welcome</h1>
-
-      <router-link to="Skills" >
-        <p>portafolio</p>
-      </router-link>
-      <router-link to="Cv"></router-link>
+  <div class="contenedor-home ">
+    <div class="izq col-6">
+      <h1>Welcome</h1> 
+      <Botones/>
+    </div>
+    <div class="der">
+      <img src="https://cdn.shopify.com/s/files/1/0590/7506/8066/products/Alien-512-Adventure_-alien_-astronomy_-monster_-outer-space_-space_-ufo_2b647b82-0d11-4200-9349-0edf691646eb.png?v=1631522821" alt="">
+    </div>
     </div>
 </template>
 <style scoped>
+@-webkit-keyframes fadein {
+  0%{
+    opacity: .4;
+
+  }
+  
+  25%{
+opacity: 1;
+  }
+  40%{
+    opacity: 1;
+
+  }
+      50%{
+      opacity: .5;
+    }
+    75%{
+      opacity: .3;
+    }
+    80%{
+      opacity: 1;
+
+    }
+    90%{
+      opacity: .6;
+    }
+    100%{
+      opacity: 1;
+    }
+
+}
+
+
+h1{
+  
+  padding-top: 2%;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  font-size: 5rem;
+
+}
+img{
+  -webkit-animation: fadein .1s ease alternate infinite;
+    -moz-animation: fadein 4s ease alternate infinite;
+    animation: fadein 2s ease infinite;
+
+  width: 80%;
+}
 .contenedor-home{
   position: relative;
-  padding: 18%;
+  display: flex;
+  width: 100%;
+}
+.izq{
+  padding-top: 10%;
 }
 
 </style>
