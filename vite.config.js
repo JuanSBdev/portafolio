79a932dcbc9d29421ee92d980aab899183cedc3f
Juan SB <juansb.dev@gmail.com>
2023-04-02 07:05:10 -0300

    asd

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/portafolio',
+  base: '/portafolio/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    assetsDir: 'assets',
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
