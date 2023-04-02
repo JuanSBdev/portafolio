90961992fbc804c5d3c191534fe9d4d00b79a3e9
Juan SB <juansb.dev@gmail.com>
2023-04-02 20:43:16 -0300

    mejoras responsive

--- src/components/TxtSk.vue
+++ src/components/TxtSk.vue
@@ -1,12 +1,12 @@
 <template >
     <!--    <img class='img-back' src="https://mystickermania.com/cdn/stickers/outer-space/astronaut-planet-balloons-512x512.png" alt="">
 -->
-<div class="containe">
-    <p class="col-4">Los lenguajes y tecnologías que utilizo para diseñar y maquetar son HTML5, CSS3, Sass, Flexbox, Grids y Bootstrap.</p>
-    <p class="col-4"> 
+<div class="containe row">
+    <p class="col-sm-4 col-12">Los lenguajes y tecnologías que utilizo para diseñar y maquetar son HTML5, CSS3, Sass, Flexbox, Grids y Bootstrap.</p>
+    <p class="col-sm-4 col-12 "> 
         Programo con JavaScript Vanilla y en uno de sus frameworks Vue.js 3, aunque tambien realicé pequeños proyectos en React y un CMS WordPress.
     </p>
-    <p class="col-4">
+    <p class="col-sm-4 col-12">
         capacitado en UX/UI, SEO y
         manejo de versiones de mi codigo con Git y utilizo GitHub para guardar mis proyectos.
         utilizacion de clientes FTP tales como FileZilla.
@@ -19,8 +19,10 @@
 @import url('https://fonts.googleapis.com/css2?family=Staatliches&display=swap');
 @media (max-width:574px) {
     .containe p{
-        font-size: 1.3rem;
+        font-size: 1.5rem;
+        color: black;
     }
+    
 }
 .containe{
     top: 6 rem;
