a512d49c4bba21e6928a1361e473f863a1dcfa59
Juan SB <juansb.dev@gmail.com>
2023-04-02 05:49:10 -0300

    rutas deploy

--- src/main.js
+++ src/main.js
@@ -1,21 +1,14 @@
-import { createApp  } from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import BootstrapVue3 from 'bootstrap-vue-3'
-
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { BootstrapVue } from 'bootstrap-vue'
 
 const app = createApp(App)
 
 app.use(router)
 
-// Optional, since every component import their Bootstrap functionality
-// the following line is not necessary
-// import 'bootstrap'
-
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
-
-//const app = createApp(App)
-app.use(BootstrapVue3)
+app.use(BootstrapVue)
 
 app.mount('#app')
