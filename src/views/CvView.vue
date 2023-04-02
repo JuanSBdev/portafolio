a8bac9468012a87ca9b8805db78e372071474093
Juan SB <juansb.dev@gmail.com>
2023-04-01 22:24:29 -0300

    rebs

---
+++ src/views/CvView.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <a href="https://drive.google.com/file/d/1n7PvEbOAO-R4SMNf86ZwE-x65o5mJZBa/view?usp=sharing">
+            <img src="https://img.freepik.com/iconos-gratis/curriculum-vitae_318-317780.jpg" alt="">
+            <p>asd</p>
+        </a>
+    </div>
+</template>
