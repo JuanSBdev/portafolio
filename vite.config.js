c50d0b91089e3ea0d7e277cea24686c11aaf26da
Juan SB <juansb.dev@gmail.com>
2023-04-02 16:05:56 -0300

    ruta vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/portafolio/',
+  base: '/portafolio/dist/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
