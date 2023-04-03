2f4a01a2c79ab711b742c3dc3921d64d8cb31838
Juan SB <juansb.dev@gmail.com>
2023-04-03 13:04:42 -0300

    mej

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -93,13 +93,23 @@ import Botones from '../components/Botones.vue';
   }
   .der img{
     width: 90%;
+    margin-left: 20%;
     transform: rotate(90deg);
     -webkit-animation: fadein .1s ease alternate infinite;
     -moz-animation: fadein .1s ease alternate infinite;
     animation: fadein 2s ease infinite;
   }
   .izq{
-    padding-top: 15%;
+    background-image: url('');
+    background-size: 5%;
+    background-repeat: repeat;
+    background-color: #9263cf10;
+    box-shadow: 0px 1px 6px 6px rgba(0,0,0,0.57);
+    padding-top: 8%;
+    width: 25%;
+    margin: 5%;
+    margin-left: 15%;
+    border-radius: 1rem;
   }
   
 }
