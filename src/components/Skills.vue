b4b084988a1667f0f3c4f07da31258648cb248b2
Juan SB <juansb.dev@gmail.com>
2023-04-07 13:53:58 -0300

    logos

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,13 +1,13 @@
 <template>
 <div class="logos-wraper">
-    <img class="logoa col-lg-1 col-sm-1 col-2 mx-3" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/768px-HTML5_logo_and_wordmark.svg.png" alt="html5 logo">
-    <img class="logob col-lg-1 col-sm-1 col-2 mx-3" src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_1280.png" alt="css3 logo">
-    <img class="logoc col-lg-1 col-sm-1 col-2 mx-3" src="https://i.pinimg.com/originals/86/a9/d2/86a9d2cf24d7950adad2ee6f49ad4b1e.png" alt="JavaScript logo">
-    <img class="logod col-lg-1 col-sm-1 col-2 mx-3" src="https://avatars.githubusercontent.com/u/22965283?s=280&v=4" alt="BV">
-    <img class="logoe col-lg-1 col-sm-1 col-2 mx-3" src="https://i0.wp.com/jrgonzalez.es/wp-content/uploads/2019/01/git-logo.png?resize=228%2C228&ssl=1" alt="git logo">
-    <img class="logof col-lg-1 col-sm-1 col-2 mx-3" src="https://www.pngarts.com/files/8/Github-Logo-PNG-Photo.png" alt="github logo">
-    <img class="logog col-lg-1 col-sm-1 col-2 mx-3" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Sass_Logo_Color.svg/2560px-Sass_Logo_Color.svg.png" alt="sass logo">
-    <img class="logoh col-lg-1 col-sm-1 col-2 mx-3" src="https://i0.wp.com/www.jacobsoft.com.mx/wp-content/uploads/2019/04/Bootstrap-Logo.png?ssl=1" alt="sass logo">
+    <img class="logoa  col-1 mx-3" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/768px-HTML5_logo_and_wordmark.svg.png" alt="html5 logo">
+    <img class="logob  col-1 mx-3" src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_1280.png" alt="css3 logo">
+    <img class="logoc  col-1 mx-3" src="https://i.pinimg.com/originals/86/a9/d2/86a9d2cf24d7950adad2ee6f49ad4b1e.png" alt="JavaScript logo">
+    <img class="logod  col-1 mx-3" src="https://avatars.githubusercontent.com/u/22965283?s=280&v=4" alt="BV">
+    <img class="logoe  col-1 mx-3" src="https://i0.wp.com/jrgonzalez.es/wp-content/uploads/2019/01/git-logo.png?resize=228%2C228&ssl=1" alt="git logo">
+    <img class="logof  col-1 mx-3" src="https://www.pngarts.com/files/8/Github-Logo-PNG-Photo.png" alt="github logo">
+    <img class="logog  col-1 mx-3" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Sass_Logo_Color.svg/2560px-Sass_Logo_Color.svg.png" alt="sass logo">
+    <img class="logoh  col-1 mx-3" src="https://i0.wp.com/www.jacobsoft.com.mx/wp-content/uploads/2019/04/Bootstrap-Logo.png?ssl=1" alt="sass logo">
   </div>
 </template>
 <style scoped>
@@ -29,5 +29,4 @@
       align-items: center;
       justify-content: center;
 }
-
 </style>
