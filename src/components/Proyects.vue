16bc9b88665ff38996f2b8695e1aa0fa6f7e238b
Juan SB <juansb.dev@gmail.com>
2023-04-09 03:34:18 -0300

    proyects

--- src/components/Proyects.vue
+++ src/components/Proyects.vue
@@ -6,32 +6,32 @@
             { Some Proyects }
         </h5>
         <div class="wraper row">
-            <div class="calculadora col-2">
+            <div class="calculadora col-6 col-sm-2">
                 <h6>Calculadora</h6>
                 <a href="https://juansbdev.github.io/appCalculadora/">
                     <img src="https://ayudahosting.online/wp-content/uploads/2021/02/031-calculator.png" alt="">
                 </a>
             </div>
-            <div class="coderhouse col-2">
+            <div class="coderhouse col-6 col-sm-2">
                 <h6>Proyecto Final Coderhouse</h6>
                 <a href="https://juansbdev.github.io/Proyecto-Coderhouse/">
                     <img src="https://img.freepik.com/iconos-gratis/hamburguesa_318-192560.jpg?w=2000" alt="">
                 </a>
             </div>
-            <div class="carta col-2">
+            <div class="carta col-6 col-sm-2">
                 <h6> App Mobile Carta 'TABAK'</h6>
                 <a href="https://juansbdev.github.io/carta/">
                     <img src="https://juansbdev.github.io/carta/img/logo2-removebg-preview.png" alt="">
                 </a>
 
             </div>
-            <div class="api col-2">
+            <div class="api col-6 col-sm-2">
                 <h6>API  Movies fetch</h6>
                 <a href="https://juansbdev.github.io/API-Movies/">
                     <img src="https://diogorodrigues.dev/assets/img/blog/fetch-api-is-new-old-version-of-ajax.jpg" alt="">
                 </a>
             </div>
-            <div class="lista col-2">
+            <div class="lista col-6 col-sm-2">
                 <h6>To Do List.</h6>
                 <a href="https://juansbdev.github.io/listaDeTareas/">
                     <img src="https://cdn-icons-png.flaticon.com/512/6194/6194029.png" alt="">
@@ -42,6 +42,7 @@
     </div>
 </template>
 <style scoped>
+
     h5{
         font-size: 4rem;
         display: flex;
