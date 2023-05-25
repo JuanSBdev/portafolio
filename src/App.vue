a9817584532f6f3ff17df9b64687ce6127caaf29
Juan SB <juansb.dev@gmail.com>
2023-05-25 17:10:24 -0300

    escarapela

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,10 @@ import { RouterLink, RouterView } from 'vue-router'
     <link rel="shortcut icon" href="https://cdn-icons-png.flaticon.com/512/2534/2534928.png" type="image/x-icon">
   </head>
 <main>
+  <div class="cara">
+    <img src="https://www.mininterior.gov.ar/asuntospoliticos/img/bandera.png" alt="Bandera argentina">
+    <p>¡Feliz Día de la Patria!</p>
+  </div>
       <router-view>
       </router-view>
 </main>
@@ -16,6 +20,22 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <style >
 @import "./assets/base.css";
+.cara{
+  width: 10%;
+  position:fixed;
+  top: 2%;
+  right: 1%;
+  display: flex;
+  flex-flow: column wrap;
+  align-content: center;
+  align-items: center;
+  z-index: 1;
+  font-size: .8rem;
+  color: rgb(23, 104, 209);
+}
+img{
+  width: 25%;
+}
 
 </style>
 
