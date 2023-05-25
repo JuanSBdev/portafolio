48b3a6a5e19d6aa5339ba69a8b60a34d3cc3331b
Juan SB <juansb.dev@gmail.com>
2023-05-25 00:28:27 -0300

    trashcode

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,6 @@
 import { ref } from 'vue';
 
 import { RouterLink, RouterView } from 'vue-router'
-import Footer from './components/Este.vue'
-import Carta from './components/Carta.vue';
-import SkillsView from './views/SkillsView.vue';
-import HomeView from './views/HomeView.vue';
 </script>
 
 <template>
@@ -13,16 +9,9 @@ import HomeView from './views/HomeView.vue';
     <link rel="shortcut icon" href="https://cdn-icons-png.flaticon.com/512/2534/2534928.png" type="image/x-icon">
   </head>
 <main>
-  <div>
       <router-view>
       </router-view>
-    
-  </div>
-  
 </main>
-<footer>
-
-</footer>
 </template>
 
 <style >
