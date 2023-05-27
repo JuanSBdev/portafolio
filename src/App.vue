ecda23f2b1815c6e7f1af177d43d12b10ae7d689
Juan SB <juansb.dev@gmail.com>
2023-05-27 20:05:16 -0300

    txt

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <script setup>
 import { ref } from 'vue';
+import { reactive } from 'vue';
+import { useRouter } from 'vue-router';
+const router = useRouter();
+const state = reactive({ link: ''  })
+const handleLink=()=>{
+  if(state.link === ''){
+    return state.link = '/en'
+  }
+  else{
+    return state.link = ''
+  }
+}
+const changeLanguage = () => {
+      const currentPath = router.currentRoute.value.path;
+      if (!currentPath.includes('EN')) {
+        const newPath = currentPath + 'EN';
+        router.push(newPath);
+      } 
+            else{
+              const pato = currentPath.replace('EN', '')
+              router.push(pato)
+            }
 
+    };
 import { RouterLink, RouterView } from 'vue-router'
+
 </script>
 
 <template>
   <head>
     <link rel="shortcut icon" href="https://cdn-icons-png.flaticon.com/512/2534/2534928.png" type="image/x-icon">
   </head>
+  
 <main>
-  <div class="cara">
-    
-  </div>
+  <router-link to="" >
+  <button @click="changeLanguage"  >{{state.link}}</button>
+</router-link>
       <router-view>
       </router-view>
 </main>
--- src/router/index.js
+++ src/router/index.js
@@ -6,13 +6,33 @@ const router = createRouter({
   routes: [
     {
       path: '/',
+      name: 'casa',
+      component: HomeView
+    },{
+      path: '/EN',
       name: 'home',
       component: HomeView
     },
   
     {
       path: '/skills',
-      name: 'skillsP',
+      name: 'skills',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/SkillsView.vue')
+    },
+    {
+      path: '/skillsEN',
+      name: 'skills-en',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/SkillViewEn.vue')
+    },
+    {
+      path: '/skills',
+      name: 'skills-es',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
