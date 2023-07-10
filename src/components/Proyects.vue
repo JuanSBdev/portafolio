277c529d4a09ae8f3b3ba61096001aa0b0c1bc52
Juan SB <juansb.dev@gmail.com>
2023-07-09 22:29:17 -0300

    redirecciones

--- src/components/Proyects.vue
+++ src/components/Proyects.vue
@@ -1,43 +1,46 @@
 
+
 <template>
     <div class="contenedor-pro">
         <h5 class="h3-con">
             { Some Proyects }
         </h5>
-        <div class="wraper row sobra">
-            <div class="calculadora col-6 col-sm-2 py-5">
-                <h6>Calculadora</h6>
-                <a href="https://juansbdev.github.io/appCalculadora/">
+        <div class="wraper sobra">
+            <div @click="redireccionarCalculadora" class="calculadora col-6 col-sm-2 py-5 card">
+                    
+                    <h6>Calculadora Javascript</h6>
+                    
                     <img src="https://ayudahosting.online/wp-content/uploads/2021/02/031-calculator.png" alt="">
-                </a>
-                <p>html- css- javascript</p>
+                    <p>html- css- javascript</p>
             </div>
-            <div class="coderhouse col-6 col-sm-2">
+            <div
+             @click="redireccionarCoder"
+             class="coderhouse col-6 col-sm-2 card">
                 <h6>PF Coderhouse</h6>
-                <a href="https://juansbdev.github.io/Proyecto-Coderhouse/">
                     <img src="https://img.freepik.com/iconos-gratis/hamburguesa_318-192560.jpg?w=2000" alt="">
-                </a>
                <p>Landing Page proyecto para coderhouse frontend con html, css, bootstrap, sass, flexbox, git, github, SEO </p> 
             </div>
-            <div class="carta col-6 col-sm-2">
+            <div
+             @click="redireccionarCarta"
+             class="carta col-6 col-sm-2 card">
                 <h6> App Mobile Carta</h6>
                 <a href="https://juansbdev.github.io/carta/">
                     <img src="https://juansbdev.github.io/carta/img/logo2-removebg-preview.png" alt="">
                 </a>
                 <p> Se utiliza para esta mobile app html5 - css3 - bootstrap - javascript</p>
             </div>
-            <div class="api col-6 col-sm-2">
+            <div
+            @click="redireccionarApi"
+             class="api col-6 col-sm-2 card">
                 <h6>Clima API React.js</h6>
-                <a href="https://beamish-trifle-472703.netlify.app/">
                     <img src="https://fairbanksmuseum.org/wp-content/uploads/2023/03/partly_sunny.png" alt="">
-                </a>
                 <p>App desarrollada en React.js - utilizando HTML5 - CSS - SASS - API REST - DEPLOYED</p>
             </div>
-            <div class="lista col-6 col-sm-2">
+            <div
+            @click="redireccionarLista"
+             class="lista col-6 col-sm-2 card">
                 <h6>To Do List.</h6>
-                <a href="https://juansbdev.github.io/listaDeTareas/">
                     <img src="https://cdn-icons-png.flaticon.com/512/6194/6194029.png" alt="">
-                </a>
                 <p>App desarrollada utilizando JavaScript Vanilla, HTML 5 Y CSS - DEPLOYED</p>
 
             </div>
@@ -85,19 +88,51 @@
     img{
         width: 60%;
     }
-    img:hover{
-        /* transform: scale(1.2); */
-    }
+    
     .contenedor-pro{
         margin-right: 1%;
     }
     .wraper{
-        width: -1%;
+        width: 90%;
+        margin: 5%;
         padding-top: 3rem;
         display: flex;
         justify-content: center;
         text-align: center;
         align-items: center;
+   
     }
+    .card{
+        text-decoration: none;
+        color: aliceblue;
+        border: 2px solid white;
+        background-color: rgb(151, 100, 221);
+        padding: 2% ;
+        margin: 2%;
+        max-height: 300px;
+        padding-bottom: 8%;
+    }
+a{
+    text-decoration: none;
+    color: aliceblue;
+}
+
 }
 </style>
+<script setup>
+
+const redireccionarCalculadora = () => {
+  window.open("https://juansbdev.github.io/appCalculadora/");
+};
+const redireccionarCalculadora = () => {
+  window.open("https://juansbdev.github.io/appCalculadora/");
+};
+const redireccionarApi = () => {
+  window.open("https://beamish-trifle-472703.netlify.app/");
+};
+const redireccionarLista = () => {
+  window.open("https://juansbdev.github.io/listaDeTareas/");
+};
+
+
+</script>
