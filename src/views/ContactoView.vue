8bb23901ef84ba77d5ef66f6d78589816c17ccc2
Juan SB <juansb.dev@gmail.com>
2023-07-13 15:53:19 -0300

    fix - btn hover

--- src/views/ContactoView.vue
+++ src/views/ContactoView.vue
@@ -4,7 +4,7 @@ import Contacto from '../components/Contacto.vue'
 </script>
 <template>
     <div class="wrapper">
-        <p class="atras">atrás</p>
+        <button class="atras">volver</button>
         <Contacto></Contacto>
     </div>
 </template>
@@ -16,7 +16,14 @@ import Contacto from '../components/Contacto.vue'
     position: absolute;
     top: 4%;
     padding: 1%;
+    color: aliceblue;
+    background-color: rgba(240, 248, 255, 0);
+    border: none;
     border-bottom: 1px solid rgb(255, 255, 255);
+}
+.atras:hover{
+    border-bottom: 1px solid rgb(215, 75, 208);
+
 }
 @media screen and (max-width: 575px) {
     .wrapper{
