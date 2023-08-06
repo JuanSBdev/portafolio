03bdb8517dc065324abf7e5b18f98e97c3b731d6
Juan SB <juansb.dev@gmail.com>
2023-08-06 20:26:25 -0300

    ft - videos

--- src/components/Videos.vue
+++ src/components/Videos.vue
@@ -1,25 +1,27 @@
 <template>
     <div class=" row justify-content-center">
-        <div class="col-11 col-xl-5 div-pokemon">
-            <p>Pokemon API</p>
+        <div class="col-11 col-xl-5 div-pokemon d-flex flex-column  align-items-center">
+            <p class="justify-content-center"> Pokemon API</p>
             <p>App fullstack developed in React / Node.js / express / PostgreSQL / Sequelize / JavaScript / css modules  </p>
             <video  class="video-pokemon col-12 mx-1" src="../assets/resources/pokemonvid1.mp4" controls></video>
         </div>
       
-        <div class="col-11 col-xl-5 div-nft">
+        <div class="col-11 col-xl-5 div-nft d-flex flex-column  align-items-center">
             <p>Landing page</p>
             <p>  100% responsive / Bootstrap / HTML5 / CSS3 / JavaScript Vanilla /
                 - Web Landing page freelance for NFT - </p>
             <video controls class="video-nft col-12 mx-1 " src="../assets/resources/sube3 (2).mp4"></video>
         </div>
 
-        <div class="col-8  div-countries">
+        <div class="col-11 col-xl-5  div-countries d-flex flex-column  align-items-center py-5">
             <p>Countries API</p>
             <p>App fullstack developed in React / express / PostgreSQL / Sequelize / JavaScript / css modules  </p>
-            <video controls class="video-country col-12 pt-5" src="../assets/resources/2023-08-05 16-05-14.mp4"></video>
+            <video controls class=" video-country col-12 pt-5" src="../assets/resources/2023-08-05 16-05-14.mp4"></video>
         </div>
     </div>
 </template>
 <style scoped>
-
+video {
+border-radius: 1rem;
+}
 </style>
