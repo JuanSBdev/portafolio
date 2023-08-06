e949739e4d4686f471665dd178ef0b590ee6e147
Juan SB <juansb.dev@gmail.com>
2023-08-06 20:18:39 -0300

    ft - vids query

--- src/components/Videos.vue
+++ src/components/Videos.vue
@@ -1,13 +1,25 @@
 <template>
-    <div class="wrapper">
-        <video  class="video-pokemon" src="../assets/resources/pokemonvid1.mp4" controls></video>
+    <div class=" row justify-content-center">
+        <div class="col-11 col-xl-5 div-pokemon">
+            <p>Pokemon API</p>
+            <p>App fullstack developed in React / Node.js / express / PostgreSQL / Sequelize / JavaScript / css modules  </p>
+            <video  class="video-pokemon col-12 mx-1" src="../assets/resources/pokemonvid1.mp4" controls></video>
+        </div>
+      
+        <div class="col-11 col-xl-5 div-nft">
+            <p>Landing page</p>
+            <p>  100% responsive / Bootstrap / HTML5 / CSS3 / JavaScript Vanilla /
+                - Web Landing page freelance for NFT - </p>
+            <video controls class="video-nft col-12 mx-1 " src="../assets/resources/sube3 (2).mp4"></video>
+        </div>
 
+        <div class="col-8  div-countries">
+            <p>Countries API</p>
+            <p>App fullstack developed in React / express / PostgreSQL / Sequelize / JavaScript / css modules  </p>
+            <video controls class="video-country col-12 pt-5" src="../assets/resources/2023-08-05 16-05-14.mp4"></video>
+        </div>
     </div>
 </template>
 <style scoped>
-.video-pokemon{
-  width: 80%;
-  display: flex;
-  margin-left: 10%;
-}
+
 </style>
