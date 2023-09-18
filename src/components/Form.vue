113d4354487d4ed704b48b2a3a903d3d76f5582a
Juan SB <juansb.dev@gmail.com>
2023-09-18 19:36:01 -0300

    formulario mostrable

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -17,24 +17,78 @@ const enviarFormulario = () => {
 </script>
 
 
-<template class="wrapper">
-  <div>
-    <label for="name">Name</label>
-    <input type="text" name="name" id="name" v-model="name">
-    <label for="email">Email</label>
-    <input type="text" name="email" id="email" v-model="email">
+<template >
+  <div class="wrapper">
+
+    <p>dejame aquí tu consulta y te responderé a la brevedad</p>
+    
+    <div class="info">
+      <div class="name">
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name" v-model="name">
+      </div>
+
+      <div class="email">
+        <label for="email">Email</label>
+        <input type="text" name="email" id="email" v-model="email">
+      </div>
+    </div>
+    
+    <textarea class="tarea" name="tarea"  v-model="tarea"></textarea>
+    
+    <button class="btn_submit" @click="enviarFormulario()">
+      send
+    </button>
+  
   </div>
 
-  <textarea name="tarea" placeholder="Consulta" v-model="tarea"></textarea>
-<button @click="enviarFormulario()">
-    Enviar 
-</button>
 </template> 
 
 
 <style scoped>
 .wrapper{
+  max-width: 50%;
     display: flex;
-    flex-flow: column;
+    flex-flow: column nowrap;
+    align-items: center;
+
+    box-shadow: 0 0 10px 0;
+    border-radius: 1rem;
+    background-color: rgba(68, 84, 190, 0.145);
+}
+
+.info{
+  display: flex;
+  width: 80%;
+}
+.name{
+  margin: 2%;
+}
+.email{
+  margin: 2%;
+  
+}
+.name input{
+background-color: rgba(202, 164, 243, 0.367);
+border-radius: 1rem;
+
+border: none;
+}
+.email input{
+  border: none;
+background-color: rgba(202, 164, 243, 0.367);
+border-radius: 1rem;
+}
+.tarea{
+  width: 80%;
+  min-height: 100px;
+  background-color: rgba(202, 164, 243, 0.367);
+  color: aliceblue;
+}
+.btn_submit{
+  border: none;
+  border-radius: 1rem;
+  margin: 1%;
+  padding: 1% 4%;
 }
 </style>
