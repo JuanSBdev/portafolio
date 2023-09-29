e53da8c2a6ce046e4d1fb2b6c9d5467613caa14f
Juan SB <juansb.dev@gmail.com>
2023-09-28 21:07:02 -0300

    FIX videos stretch

--- src/components/Videos.vue
+++ src/components/Videos.vue
@@ -1,7 +1,8 @@
 <template>
     <div class=" row justify-content-center wrapper">
         
-        <div v-motion-pop-visible class="col-11 col-xl-5  div-countries d-flex flex-column  align-items-center py-5">
+        <div class="primeros_videos d-flex row  justify-content-center ">
+            <div v-motion-pop-visible class="col-11 col-xl-5  div-countries d-flex flex-column  align-items-center py-5">
             <p class="videoTitles">e-commerce D'Lujo</p>
             <p>E-Commerce developed in React.js using JavaScript, HTML5, CSS Modules, Redux, Bootstrap, Express.JS, PostgreSQL, Sequelize, MercadoPago JWT ,Auth0, + </p>
             <video controls class=" video-country col-12 pt-5" src="../assets/resources/ecommerce-PF-grupo08.mp4"></video>
@@ -10,6 +11,7 @@
             <p class="videoTitles">Countries API</p>
             <p>App fullstack developed in React using NodeJS, expressJS, PostgreSQL, Sequelize, JavaScript, Redux, CSS modules  </p>
             <video controls class=" video-country col-12 pt-5" src="../assets/resources/presentacionCountries.mp4"></video>
+        </div>
         </div>
         <div v-motion-pop-visible class="col-11 col-xl-5 div-pokemon d-flex flex-column  align-items-center">
             <p class="videoTitles"> Pokemon API</p>
@@ -30,11 +32,19 @@
     font-size: 1.2rem;
     font-family: 'Ubuntu', sans-serif;
     color: rgb(247, 186, 247);
+    align-items: stretch;
 
 }
 video {
 border-radius: 1rem;
 }
+
+
+.primeros_videos{
+    align-items: end;
+}
+
+
 .videoTitles{
     font-size: 2rem;
 }
