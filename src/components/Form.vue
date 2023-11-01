413597b9629f0ab14e4fdde41fee4ca6444cf406
Juan SB <juansb.dev@gmail.com>
2023-10-31 21:22:36 -0300

    FT - FORM bilingue

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -51,10 +51,37 @@ const datos = {
 
 <template >
 
-<section class=" flex flex-row justify-center items-center dark:bg-gray-900  w-100" >
+<section v-if="i18n.global.locale === 'es'" class=" flex flex-row justify-center items-center dark:bg-gray-900  w-100" >
   <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md ">
-      <p  v-if="i18n.global.locale === 'es'" class="mb-8 lg:mb-6 font-light text-center text-texto-col dark:text-gray-400 sm:text-xl">dejame aquí tu consulta y te responderé a la brevedad.</p>
-      <p  v-else class="mb-8 lg:mb-6 font-light text-center text-texto-col dark:text-gray-400 sm:text-xl">get in touch I'll respond as quickly as possible.</p>
+      <p   class="mb-8 lg:mb-6 font-light text-center text-texto-col dark:text-gray-400 sm:text-xl">dejame aquí tu consulta y te responderé a la brevedad.</p>
+      
+
+      <div  class="space-y-5 ">
+          <div>
+              <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Tu email</label>
+              <input type="email" name="email" v-model="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@mail.com" required>
+          </div>
+          <div>
+              <label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Asunto</label>
+              <input type="text" id="subject" v-model="name" class="block p-1.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Como puedo ayudarte?" required>
+          </div>
+          <div class="sm:col-span-2">
+              <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Tu mensaje</label>
+              <textarea id="message" name="message" v-model="message" rows="4" class="block p-1 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Deja aquí tu consulta.."></textarea>
+          </div>
+
+          <button v-if="!loading" @click="enviarFormulario()" type="submit" class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Enviar</button>
+
+          <button v-else  type="submit" class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Enviando..</button>
+
+      </div>
+  </div>
+</section>
+
+<section v-else class=" flex flex-row justify-center items-center dark:bg-gray-900  w-100" >
+  <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md ">
+      
+      <p  class="mb-8 lg:mb-6 font-light text-center text-texto-col dark:text-gray-400 sm:text-xl">get in touch I'll respond as quickly as possible.</p>
 
       <div  class="space-y-5 ">
           <div>
